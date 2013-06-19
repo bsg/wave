49110f05d10a7749c14942aad6da3d7867b90b35
Cem Saldirim <bytesong@gmail.com>
2013-06-19 15:33:26 +0300

    Integrated the global parameters where necessary

--- src/channel.c
+++ src/channel.c
@@ -1,6 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#include "globals.h"
 #include "channel.h"
 #include "logger.h"
 
@@ -11,25 +13,47 @@
         The array list implementation must not be replaced with i.e. a linked
         list for cache coherency
 */
-channel_t channels[CHANNEL_LIMIT];
+
+unsigned int channel_limit;
+unsigned int subscriber_limit;
+channel_t *channels;
 
 void ch_init_all_channels() {
+    channel_limit = GLOBAL->max_channel_count;
+    channels = calloc(channel_limit, sizeof(channel_t));
+    if(channels == NULL) {
+        printf("Failed to allocate space for %d channels\n",
+            channel_limit);
+        /* TODO: Fail gracefully */
+    }
+
     int i;
-    for(i = 0; i < CHANNEL_LIMIT; i++) {
+    for(i = 0; i < channel_limit; i++) {
         ch_init(i);
     }
 }
 
 void ch_init(int ch) {
     int i;
-    for(i = 0; i < SUBSCRIBER_LIMIT; i++)
+    
+    subscriber_limit = GLOBAL->max_subscribers_per_channel;    
+
+    channels[ch].subscribers = calloc(subscriber_limit, sizeof(int));
+
+    if(channels[ch].subscribers == NULL) {
+        printf("Failed to allocate space for %d subscribers\n",
+            subscriber_limit);
+        /* TODO: Fail gracefully */
+    }
+
+    for(i = 0; i < subscriber_limit; i++)
         channels[ch].subscribers[i] = 0;
 }
 
 inline int ch_subscribe(int ch, int fd) {
     /* TODO: Prevent multiple subscriptions from the same connection to the same channel */
     int i;
-    for(i = 0; i < SUBSCRIBER_LIMIT; i++) {
+    for(i = 0; i < subscriber_limit; i++) {
         if(channels[ch].subscribers[i] == 0) {
             channels[ch].subscribers[i] = fd;
             return CH_SUCCESS;
@@ -40,14 +64,14 @@ inline int ch_subscribe(int ch, int fd) {
 
 inline void ch_unsubscribe_from_all(int fd) {
     int i;
-    for(i = 0; i < CHANNEL_LIMIT; i++) {
+    for(i = 0; i < channel_limit; i++) {
         ch_unsubscribe(i, fd);
     }
 }
 
 inline int ch_unsubscribe(int ch, int fd) {
     int i;
-    for(i = 0; i < SUBSCRIBER_LIMIT; i++) {
+    for(i = 0; i < subscriber_limit; i++) {
         if(channels[ch].subscribers[i] == fd) {
             channels[ch].subscribers[i] = 0;
             return CH_SUCCESS;
@@ -61,7 +85,7 @@ inline int ch_publish(int ch, int fd, char *data, int size) {
     int i;
     char header[32];
 
-    for(i = 0; i < SUBSCRIBER_LIMIT; i++) {
+    for(i = 0; i < subscriber_limit; i++) {
         if(channels[ch].subscribers[i] != 0) {
             sprintf(header, "msg %d\n", ch);
             srv_writeall(channels[ch].subscribers[i], header, strlen(header));
--- src/main.c
+++ src/main.c
@@ -8,18 +8,15 @@
 #include "channel.h"
 #include "logger.h"
 
-#define BUFFER_SIZE 1024
-
-unsigned int buffer_size;
+size_t buffer_size;
 char *buffer;
 
 int hnd_read(srv_t *ctx, int fd) {
-	char buffer[BUFFER_SIZE];
 	int nread;
 
-    bzero(buffer, BUFFER_SIZE); /* TODO: Profile this */
+    bzero(buffer, buffer_size); /* TODO: Profile this */
 
-	nread =	srv_readall(fd, buffer, BUFFER_SIZE);
+	nread =	srv_readall(fd, buffer, buffer_size);
 	if(!nread) {
 		printf("Connection dropped - FD: %d\n", fd);
         ch_unsubscribe_from_all(fd);
@@ -41,6 +38,7 @@ void sigpipe_handler(int signum) {
 }
 
 void set_default_globals() {
+    GLOBAL->port                        = 9999;
     GLOBAL->tcp_buffer_size             = 1024;
     GLOBAL->message_buffer_size         = 1024;
     GLOBAL->max_channel_count           = 256;
@@ -48,25 +46,39 @@ void set_default_globals() {
 }
 
 void print_globals() {
-    printf("TCP read buffer size            : %d\n", GLOBAL->tcp_buffer_size);
-    printf("Message buffer size             : %d\n", GLOBAL->message_buffer_size);
-    printf("Maximum channel count           : %d\n", GLOBAL->max_channel_count);
-    printf("Maximum subscribers per channel : %d\n", GLOBAL->max_subscribers_per_channel);
+    printf("TCP read buffer size            : %d\n",
+        GLOBAL->tcp_buffer_size);
+    printf("Message buffer size             : %d\n",
+        GLOBAL->message_buffer_size);
+    printf("Maximum channel count           : %d\n",
+        GLOBAL->max_channel_count);
+    printf("Maximum subscribers per channel : %d\n",
+        GLOBAL->max_subscribers_per_channel);
 }
 
-void main() {
+char *init_message_buffers(size_t size) {
+    buffer_size = size;
+    buffer = (char *) calloc(size, sizeof(char));
+    return buffer;
+}
+
+int main() {
+    char port[8];
+
     /* BEWARE! Deleting the following line will open the gates of hell.
      * We might get SIGPIPE when trying to write to a dropped socket.
      * This is to prevent the signal from fuxoring the whole process.
      */
     signal(SIGPIPE, sigpipe_handler);
 
-    /* Init the global value store */
+    /* Initialize the global value store */
     globals_init();
     set_default_globals();
     
+    /* libserv context */
 	srv_t ctx;
 
+    /* Initialize networking subsystem */
 	srv_init(&ctx);
 	srv_hnd_accept(&ctx, hnd_accept);
 	srv_hnd_read(&ctx, hnd_read);
@@ -74,7 +86,16 @@ void main() {
     printf("Initializing wave with the following parameters...\n");
     print_globals();
 
+    if(init_message_buffers(GLOBAL->message_buffer_size) == NULL) {
+        printf("Failed to allocate %d bytes for message buffer!\n",
+            GLOBAL->message_buffer_size);
+        return 0;
+    }
+
     ch_init_all_channels();
 
-	srv_run(&ctx, NULL, "9999");
+    sprintf(port, "%d", GLOBAL->port);
+    printf("Listening on port %s\n", port);
+	srv_run(&ctx, NULL, port);
 }
+
